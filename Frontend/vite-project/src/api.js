a122b6f0331cfeb2caf418086b11975f6293f279
abhishek6164 <abhishekpipriye2507@gmail.com>
2025-10-08 17:56:23 +0530

    initial commit

--- Frontend/vite-project/src/api.js
+++ Frontend/vite-project/src/api.js
@@ -1,4 +1,8 @@
+import axios from "axios";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // ✅ VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL, // ✅ correct
   withCredentials: true,
 });
+
+export default api; // ✅ default export
